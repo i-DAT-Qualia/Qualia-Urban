fd12bfe7da9f985eec8d989a977a69b56b61ef77
Chris Hunt <mail@thisisthechris.co.uk>
2015-04-27 12:40:48 +0100

    Adds Device Location Update

--- app/dashboard/content/devices.py
+++ app/dashboard/content/devices.py
@@ -9,11 +9,26 @@ from django.utils import timezone
 
 from sensors.models import *
 
+from django.forms import ModelForm
+
 import numpy
 import datetime
 import json
 
 from qualia.tools.decorators import dashboard_level_required
+
+from leaflet.forms.widgets import LeafletWidget
+
+
+class LocationUpdateForm(ModelForm):
+
+    class Meta:
+        model = Location
+        fields = ['gps']
+
+        widgets = {
+            'gps': LeafletWidget(),
+        }
 
 
 def can_edit(thing, user):
@@ -34,13 +49,34 @@ def list(request):
 @dashboard_level_required
 def detail(request, id):
     device = get_object_or_404(Thing, id=id)
-    #loc = Location.objects.filter(thing=device).order_by('-added')
     channel = Channel.objects.filter(thing=device).order_by('-added')
-    #lastloc = loc.last()
 
     return render_to_response('content/devices/detail.html', {
         'device': device,
-        #'lastloc': device.last_location(),
-        #'loc': device.location_list(),
         'can_edit': can_edit(device, request.user),
+    }, context_instance=RequestContext(request))
+
+
+@dashboard_level_required
+def update_location(request, id):
+    device = get_object_or_404(Thing, id=id)
+    form = LocationUpdateForm()
+    success = False
+
+    if request.method == "POST":
+        form = LocationUpdateForm(request.POST)
+
+        if form.is_valid():
+            new_item = form.save(commit=False)
+            new_item.thing = device
+            new_item.save()
+            success = True
+
+        if success:
+            return redirect('/dashboard/content/devices/'+str(id)+'/')
+
+    return render_to_response('content/devices/update_location.html', {
+        'device': device,
+        'form': form,
+        'success': success
     }, context_instance=RequestContext(request))
--- app/dashboard/urls.py
+++ app/dashboard/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 
 urlpatterns = [
 
+    url(r'content/devices/(?P<id>[^/]+)/location', 'dashboard.content.devices.update_location'),
     url(r'content/devices/(?P<id>[^/]+)/', 'dashboard.content.devices.detail'),
     url(r'content/devices/', 'dashboard.content.devices.list'),
 
