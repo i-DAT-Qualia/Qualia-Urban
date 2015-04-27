2f9d5f114dd287ef8fa475ec0845a5da91458d4a
Chris Hunt <mail@thisisthechris.co.uk>
2015-04-27 01:50:47 +0100

    Adds API Info page

---
+++ app/dashboard/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import Context, loader, RequestContext
+from django.template.loader import render_to_string
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.shortcuts import render_to_response, get_object_or_404, redirect
+from django.utils import timezone
+
+from sensors.models import *
+
+import numpy
+import datetime
+import json
+
+from qualia.tools.decorators import dashboard_level_required
+
+
+@dashboard_level_required
+def api_info(request):
+    return render_to_response('info/api.html', {
+    }, context_instance=RequestContext(request))
