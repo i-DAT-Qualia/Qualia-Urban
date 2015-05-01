0d47175ec83f8df582a5351a8c88f03d8c5ca254
Chris Hunt <mail@thisisthechris.co.uk>
2015-05-01 16:08:39 +0100

    Moves Photo dash views

--- app/dashboard/urls.py
+++ app/dashboard/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     url(r'content/devices/(?P<id>[^/]+)/location', 'dashboard.content.devices.update_location'),
     url(r'content/devices/(?P<id>[^/]+)/', 'dashboard.content.devices.detail'),
     url(r'content/devices/', 'dashboard.content.devices.list'),
-    url(r'media/photos/add/', 'dashboard.media.photos.upload'),
-    url(r'media/photos/success/', 'dashboard.media.photos.success'),
+    url(r'media/photos/add/', 'dashboard.content.photos.upload'),
+    url(r'media/photos/success/', 'dashboard.content.photos.success'),
 
     url(r'information/api/', 'dashboard.views.api_info'),
 
