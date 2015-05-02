7b148e10c1cdab90ab4b7f87a3aa6c31f39edef5
Chris Hunt <mail@thisisthechris.co.uk>
2015-05-02 15:35:44 +0100

    Adds thumbnails and GPS to photo admin

--- app/config/settings.py
+++ app/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.gis',
     'django.contrib.postgres',
     'tastypie',
+    'imagekit',
     'leaflet',
     'provider',
     'provider.oauth2',
--- app/media/admin.py
+++ app/media/admin.py
@@ -1,9 +1,16 @@
 from django.contrib.gis import admin
 from models import *
 
+from imagekit.admin import AdminThumbnail
 
 class MetaAdmin(admin.OSMGeoAdmin):
     search_fields = ['id']
+    list_display = ('__str__', 'thumbnail', 'gps')
+    thumbnail = AdminThumbnail(image_field='thumb')
+
+
+class LinkAdmin(admin.OSMGeoAdmin):
+    search_fields = ['id']
 
 admin.site.register(Photo, MetaAdmin)
-admin.site.register(FileLink, MetaAdmin)
+admin.site.register(FileLink, LinkAdmin)
