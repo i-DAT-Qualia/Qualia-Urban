a275d43c044063a8f4561e0b08c7d0e0da0899a4
Chris Hunt <mail@thisisthechris.co.uk>
2016-04-12 16:06:40 +0100

    Disables add photo view

--- app/dashboard/urls.py
+++ app/dashboard/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     url(r'content/devices/(?P<id>[^/]+)/location', 'dashboard.content.devices.update_location'),
     url(r'content/devices/(?P<id>[^/]+)/', 'dashboard.content.devices.detail'),
     url(r'content/devices/', 'dashboard.content.devices.list'),
-    url(r'media/photos/add/', 'dashboard.content.photos.upload'),
+    #url(r'media/photos/add/', 'dashboard.content.photos.upload'),
     url(r'media/photos/success/', 'dashboard.content.photos.success'),
 
     url(r'information/api/', 'dashboard.views.api_info'),
