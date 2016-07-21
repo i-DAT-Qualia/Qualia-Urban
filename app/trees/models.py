5c20cee870a22c978ea14f3db057c6ee3af319af
Chris Hunt <mail@thisisthechris.co.uk>
2016-07-21 23:47:06 +0100

    Adds Datapoint models

--- app/trees/models.py
+++ app/trees/models.py
@@ -6,6 +6,12 @@ from django.contrib.postgres.fields import ArrayField
 from django.contrib.postgres.fields import JSONField
 import uuid
 
+from qualia.tools.uploads import get_image_path
+
+from imagekit.models import ImageSpecField
+from imagekit.processors import Thumbnail, ResizeToFit
+
+
 
 class TreesMeta(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -56,6 +62,93 @@ class Tree(TreesMeta):
 
     def __unicode__(self):
         return self.name
+
+
+class DataPoint(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    tree = models.ForeignKey(Tree)
+
+    added = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True, blank=True, null=True)
+    recorded = models.DateTimeField()
+
+    author = models.CharField(blank=True, null=True, max_length=250, default="Anon.")
+
+    class Meta:
+        abstract = True
+
+
+class Reading(DataPoint):
+    type_choices = (
+        ('A', 'Height of Tree'),
+        ('B', 'Spread of Crown'),
+        ('C', 'Diameter at Breast Height'),
+        ('D', 'Clear Stem Height'),
+        ('O', 'Other'),
+    )
+
+    type = models.CharField(
+        default='O',
+        choices=type_choices,
+        max_length=1
+    )
+
+    value = models.FloatField()
+
+    def __unicode__(self):
+        return self.tree.name
+
+
+class Photo(DataPoint):
+    image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
+    large = ImageSpecField(
+        source='image',
+        processors=[Thumbnail(width=640, height=440, upscale=True)],
+        format='JPEG',
+        options={'quality': 100}
+    )
+    thumb = ImageSpecField(
+        source='image',
+        processors=[Thumbnail(width=320, height=220, upscale=True)],
+        format='JPEG',
+        options={'quality': 100}
+    )
+
+    def __unicode__(self):
+        return self.tree.name
+
+
+class Story(DataPoint):
+    info = models.TextField(blank=True, null=True)
+
+    def __unicode__(self):
+        return self.tree.name
+
+
+class Report(DataPoint):
+    type_choices = (
+        ('A', 'Site History'),
+        ('B', 'Form'),
+        ('C', 'Condition'),
+        ('D', 'Nearby Obstructions'),
+        ('E', 'Past Management'),
+        ('F', 'Recommendations'),
+        ('O', 'Other'),
+    )
+
+    type = models.CharField(
+        default='O',
+        choices=type_choices,
+        max_length=1
+    )
+
+    info = models.TextField(blank=True, null=True)
+
+    def __unicode__(self):
+        return self.tree.name
+
+
+
 '''
 class TreesMeta:
 
